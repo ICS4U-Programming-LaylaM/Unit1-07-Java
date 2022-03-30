0adb192cfa2d877a60ea47a0f25bce5f3436d1d5
Ubuntu <ubuntu@ip-172-31-12-208.ec2.internal>
2022-03-30 03:57:38 +0000

    Removed unnecessary import

--- ClassMarks.java
+++ ClassMarks.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.Arrays;
 
 /**
 * This program reads from two files, generates random marks
